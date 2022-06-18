330866e3302ba486b3bff02f6c364fca93bac9f0
Austin Borck <austinborck98@gmail.com>
2022-06-18 10:52:06 -0600

    Completed MakeChange project

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 
 ## Description
-
+My project, the Change Maker is similar to a cash register in taking in a purchase amount and payment amount and then providing exact change. My change maker will tell the user the exact bills to produce in the highest denomination from top to bottom in a list format, this is easier to read quickly and makes it possible to save time by assisting the user.        
 
 ## Technologies used
-
+I used if statements, scripting a double provided by the user into an int that is printed out onto the console, the % operator and all of it wrapped inside a while loop dictated by a boolean.
 
 ## What I learned
+I learned to focus on where you set your operators, I was stuck on the issue of printing something like "0 ten dollar bills" for quite some time because I did not assign the remainder of the change to the variable change prior to the if statement. 
+
+I also ran into the issue of an infinite loop when I left out the change variable being re assigned to change = change % the specified amount in each statement, which I solved by pushing to github then not being afraid of breaking my code and loosing progress while attempting some drastic changes. 
+
+The final big thing I learned is playing around with formatting and adding a new line or other things to my sysout's to make the console look neat and organized. 
+
+I completed this project with a lot of trial and error. I hope to refine my skills and get the small things right the first time in order to make myself a better developer and more valuable in the eyes of potential employers.
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -15,47 +15,46 @@ public class MakeChange {
 		while (registerOn) {
 			System.out.println("Enter total price.");
 			price = scanner.nextDouble();
+			scanner.nextLine();
 			System.out.println("Enter the customers payment.");
 			payment = scanner.nextDouble();
-
+			scanner.nextLine();
 			price = price * 100;
 			payment = payment * 100;
 			change = payment - price;
-				System.out.println("\nResult: ");
+				System.out.println("Result: ");
 
-				if (change / 2000 >= 1) {
+				if ((change / 2000) >= 1) {
 					double twenties = change / 2000;
-					System.out.print((int) twenties + " twenty dollar bill(s) ");
-					total = total + twenties;
-				} if (change / 1000 >= 1) {
+					System.out.println((int) twenties + " twenty dollar bill(s) ");
 					change = change % 2000;
+				} if ((change / 1000) >= 1) {
 					double tens = change / 1000;
-					System.out.print((int) tens + " ten dollar bill(s) ");
-					total = total + tens;
-				} if (change / 500 >= 1) {
+					System.out.println((int) tens + " ten dollar bill(s) ");
 					change = change % 1000;
+				} if ((change / 500) >= 1) {
 					double fives = change / 500;
-					System.out.print((int) fives + " five dollar bill(s) ");
-				} if (change / 100 >= 1) {
+					System.out.println((int) fives + " five dollar bill(s) ");
 					change = change % 500;
+				} if ((change / 100) >= 1) {
 					double ones = change / 100;
-					System.out.print((int) ones + " one dollar bill(s) ");
-				} if (change / 25 >= 1) {
+					System.out.println((int) ones + " one dollar bill(s) ");
 					change = change % 100;
+				} if ((change / 25) >= 1) {
 					double quarters = change / 25;
-					System.out.print((int) quarters + " quarter(s) ");
-				} if (change / 10 >= 1) {
+					System.out.println((int) quarters + " quarter(s) ");
 					change = change % 25;
+				} if ((change / 10) >= 1) {
 					double dimes = change / 10;
-					System.out.print((int) dimes + " dime(s) ");
-				} if (change / 5 >= 1) {
+					System.out.println((int) dimes + " dime(s) ");
 					change = change % 10;
+				} if ((change / 5) >= 1) {
 					double nickles = change / 5;
-					System.out.print((int) nickles + " nickle(s) ");
-				} if (change / 1 >= 1) {
+					System.out.println((int) nickles + " nickle(s) ");
 					change = change % 5;
+				} if ((change / 1) >= 1) {
 					double pennies = change / 1;
-					System.out.print((int) pennies + " penny/pennies ");
+					System.out.println((int) pennies + " penny/pennies ");
 				} if (price == payment) {
 					System.out.println("Nothing, thank you for giving exact change!");
 				} else if (payment < price){
